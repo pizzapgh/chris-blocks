5fa954b136eaabdcfe84198735077a5cb637dcd2
pizzapgh <charligrosspgh@gmail.com>
2023-06-28 09:43:45 -0400

    revisions block icon

--- build/callout-block/index.asset.php
+++ build/callout-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '74417d97c91bfdcc3d6f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c2ed72bf17e349437c82');
